de72c2acc13bf45717eea592d1cd62a203d0160d
informati <46190977+Matthieusz@users.noreply.github.com>
2025-07-08 17:26:17 +0200

    update the styling of the loader

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -2,8 +2,9 @@ import { Loader2 } from "lucide-react";
 
 export default function Loader() {
 	return (
-		<div className="flex h-full items-center justify-center pt-8">
-			<Loader2 className="animate-spin" />
+		<div className="flex h-full flex-col items-center justify-center pt-8">
+			<Loader2 className="size-12 animate-spin text-foreground" />
+			<span className="mt-2 text-foreground">Loading...</span>
 		</div>
 	);
 }
