101c38ae1b0dccc36a2fad19d5a82235274fd9e1
informati <46190977+Matthieusz@users.noreply.github.com>
2025-07-10 19:46:51 +0200

    fix: update Loader component to use full screen height and background color

--- apps/web/src/components/loader.tsx
+++ apps/web/src/components/loader.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loader() {
 	return (
-		<div className="flex h-full flex-col items-center justify-center pt-8">
+		<div className="flex h-screen flex-col items-center justify-center bg-background pt-8">
 			<Loader2 className="size-12 animate-spin text-foreground" />
 			<span className="mt-2 text-foreground">Loading...</span>
 		</div>
